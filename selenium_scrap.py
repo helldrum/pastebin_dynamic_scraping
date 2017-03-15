6798dc32fbf665e9ea40aa34e5fb665a7e0fa1d1
Jonathan CHARDON <jonathanchardon@hotmail.fr>
2017-03-15 22:03:58 +0100

    prepare loop logic, need to find a way again google capcha

--- selenium_scrap.py
+++ selenium_scrap.py
@@ -8,49 +8,45 @@ from selenium.webdriver.support.ui import WebDriverWait
 import re
 import requests
 
+def scrap_pastebin_suffix( browser, suffix_clean):
+  ## scrap and extract pastebin suffix ex pastebin.com/e2tYTu45 -> e2tYTu45
+  soup = BeautifulSoup(browser.page_source, 'html.parser')
+  for link in soup.find_all('cite'):
+    pastebin_suffix = link.string.split("/",1)[1]
+    if re.search('([A-Za-z0-9]){8}', pastebin_suffix):
+      suffix_clean.append(pastebin_suffix)
+
+def prepare_pastebin_raw_links(suffix_clean, pastebin_raw): 
+  for suffix in suffix_clean:
+    pastebin_raw.append("http://pastebin.com/raw/" + suffix)
+
+
+#TODO main function + opt parser
+
 search = "python"
 page = 0
 url = "https://www.google.fr/?gfe_rd=cr&ei=S4rJWPnnHcWEaNjju7AH&gws_rd=ssl#q=site:pastebin.com+" + search + "&start="
 suffix_clean = []
 pastebin_raw = []
 
+#init selenium driver
 browser = webdriver.Firefox()
-
-browser.get(url)
+ 
 time.sleep(5)
-
-## scrap and extract pastebin suffix ex pastebin.com/e2tYTu45 -> e2tYTu45
-soup = BeautifulSoup(browser.page_source, 'html.parser')
-for link in soup.find_all('cite'):
-  pastebin_suffix = link.string.split("/",1)[1]
-  if re.search('([A-Za-z0-9]){8}', pastebin_suffix):
-    suffix_clean.append(pastebin_suffix)
-
-#for suffix in suffix_clean:
-#  pastebin_raw.append("http://pastebin.com/raw/" + suffix)
-
-#for url in pastebin_raw:
-#  print "\n+++++++++++++++++++++++++" + url + "+++++++++++++++++++++\n"
-#  result = requests.get(url)
-#  time.sleep(1)
-#  print result.text.encode('ascii', 'ignore')
-#  print "\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+browser.get(url)
+scrap_pastebin_suffix(browser, suffix_clean)
 
 
-page += 10
 
-browser.get(url+str(page))
-time.sleep(2)
+## TODO need to find a way to stop and save when google capcha gotcha
+#while true:
 
-soup = BeautifulSoup(browser.page_source, 'html.parser')
-for link in soup.find_all('cite'):
-  pastebin_suffix = link.string.split("/",1)[1]
-  if re.search('([A-Za-z0-9]){8}', pastebin_suffix):
-    suffix_clean.append(pastebin_suffix)
+  page += 10
 
+  browser.get(url+str(page))
+  time.sleep(10)
+  scrap_pastebin_suffix(browser, suffix_clean)
 
-for suffix in suffix_clean:
-  pastebin_raw.append("http://pastebin.com/raw/" + suffix)
+prepare_pastebin_raw_links(suffix_clean, pastebin_raw)
 
-for link in pastebin_raw:
-  print link
+## TODO print in a file or in terminal the good stuff :)
