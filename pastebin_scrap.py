4792cecb3ffc111384a5388a69002d487d8fe4c4
Jonathan CHARDON <jonathanchardon@hotmail.fr>
2017-03-15 19:47:59 +0100

    minimal scrap OK

--- pastebin_scrap.py
+++ pastebin_scrap.py
@@ -1,14 +1,16 @@
-# venv/bin/python
+#/usr/bin/python
+#coding:utf8
 
+import dryscrape
 from bs4 import BeautifulSoup
-import requests
- 
-#url = input('Enter a website to extract URL\'s from: ')
-r = requests.get('http://pastebin.com/archive')
- 
-data = r.text
- 
-soup = BeautifulSoup(data, 'html.parser')
- 
+
+search="python"
+url="https://www.google.com/#?q=site:pastebin.com+" + search + "&*"
+
+session = dryscrape.Session()
+session.visit(url)
+response = session.body()
+soup = BeautifulSoup(response, "lxml")
+
 for link in soup.find_all('a'):
-    print(link.string, link.get('href'))
+  print(link.string, link.get('href'))
