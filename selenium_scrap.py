6e7f2762b7f71b47ba2a8de15c43331a8fc07817
Jonathan CHARDON <jonathanchardon@hotmail.fr>
2017-03-16 21:48:37 +0100

    improve logic, need serious refactoring :)

--- selenium_scrap.py
+++ selenium_scrap.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import time
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
 import re
 import requests
 
@@ -20,9 +21,6 @@ def prepare_pastebin_raw_links(suffix_clean, pastebin_raw):
   for suffix in suffix_clean:
     pastebin_raw.append("http://pastebin.com/raw/" + suffix)
 
-
-#TODO main function + opt parser
-
 search = "python"
 page = 0
 url = "https://www.google.fr/?gfe_rd=cr&ei=S4rJWPnnHcWEaNjju7AH&gws_rd=ssl#q=site:pastebin.com+" + search + "&start="
@@ -34,19 +32,39 @@ browser = webdriver.Firefox()
  
 time.sleep(5)
 browser.get(url)
+
+time.sleep(3)
 scrap_pastebin_suffix(browser, suffix_clean)
 
+next_button_exist = browser.find_element_by_css_selector("#pnnext > span")
 
+while next_button_exist :
 
-## TODO need to find a way to stop and save when google capcha gotcha
-#while true:
-
+  try:
+    next_button_exist = browser.find_element_by_css_selector("#pnnext > span")
+  except NoSuchElementException:
+    next_button_exist = None
+  
   page += 10
-
   browser.get(url+str(page))
-  time.sleep(10)
+  print "browse"+ url+str(page)
+  time.sleep(30)
   scrap_pastebin_suffix(browser, suffix_clean)
+  prepare_pastebin_raw_links(suffix_clean, pastebin_raw)
 
-prepare_pastebin_raw_links(suffix_clean, pastebin_raw)
+  for raw in pastebin_raw:
+    print raw
+    
+    file = open("testfile.txt","a")
+    file.write("\n+++++++++++++++++++++++++"+raw+"%+++++++++++++++++++++\n")
 
-## TODO print in a file or in terminal the good stuff :)
+    result = requests.get(raw)
+    time.sleep(10)
+
+    file.write(result.text.encode('ascii', 'ignore'))
+    file.write("\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
+    file.close()
+
+  suffix_clean = []
+  pastebin_raw = []
+
